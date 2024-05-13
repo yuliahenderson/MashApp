7509b78db6042e769a736e78b84f28f67356426d
Julia Henderson <yulia.p.henderson@gmail.com>
2024-05-13 13:41:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,6 @@
 
-// // A simplification of a scroll-based theme-color changer
-// window.addEventListener('scroll', () => {
-//   const metaTag = document.querySelector('meta[name="theme-color"]');
-//   if (window.scrollY / window.innerHeight > 0.8) {
-//     metaTag.setAttribute("content", "#000000");
-//   } else {
-//     metaTag.setAttribute("content", "#ffffff");
-//   }
-// }, false);
-document.addEventListener('DOMContentLoaded', function() {
-    var themeColorMeta = document.getElementById('theme-color-meta');
 
-    function isScrolledTo95PercentOfPage2() {
-        var page2 = document.getElementById('page2');
-        if (page2) {
-            var page2Height = page2.offsetHeight;
-            var page2Top = page2.offsetTop;
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            var scrollThreshold = page2Top + (page2Height * 0.95); // 95% of page2 height
 
-            return scrollTop >= scrollThreshold;
-        } else {
-            console.error('Page 2 element not found.');
-            return false;
-        }
-    }
-
-    function isScrolledTo25PercentOfPage3() {
-        var page3 = document.getElementById('page3');
-        if (page3) {
-            var page3Top = page3.offsetTop;
-            var page3Height = page3.offsetHeight;
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            var scrollThreshold = page3Top + (page3Height * 0.25); // 25% of page3 height
-
-            return scrollTop >= scrollThreshold;
-        } else {
-            console.error('Page 3 element not found.');
-            return false;
-        }
-    }
-
-    function handleScroll() {
-        var scrolledTo95PercentOfPage2 = isScrolledTo95PercentOfPage2();
-        var scrolledTo25PercentOfPage3 = isScrolledTo25PercentOfPage3();
-
-        if (scrolledTo95PercentOfPage2 && !scrolledTo25PercentOfPage3) {
-            // Change meta tag color to green when scrolled to 95% of page2 and not scrolled to 25% of page3
-            themeColorMeta.setAttribute('content', '#38FF27');
-        } else {
-            // Reset meta tag color to white
-            themeColorMeta.setAttribute('content', '#ffffff');
-        }
-    }
-
-    // Initial call to handleScroll() to set initial state based on page load position
-    handleScroll();
-
-    // Attach scroll event listener to the window
-    window.addEventListener('scroll', handleScroll);
-});
 
         document.addEventListener("DOMContentLoaded", function () {
         const slider = document.querySelector('.slider');
@@ -86,65 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
         setInterval(nextSlide, 5000);
     });
 
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Get reference to the scrollable container element
-//     var container = document.querySelector('.container');
-
-//     // Get reference to the button in the fixed header
-//     var header = document.getElementById('fixedHeader');
-//     var mlogo = document.getElementById('mlogo');
-//     var button = document.getElementById('scrollButton');
-//    var nav = document.querySelector('nav');
-//     var logo = document.getElementById('logo');
-
-
-//     // Store the original logo source
-//     var originalLogoSrc = mlogo.src;
-
-//     // Function to check if scrolled to the second page
-//     function isScrolledToSecondPage() {
-//         var secondPage = document.querySelector('.pages.page2');
-//         if (secondPage) {
-//             var containerHeight = container.clientHeight;
-//             var scrollTop = container.scrollTop;
-//             var secondPageOffsetTop = secondPage.offsetTop;
-
-//             return scrollTop >= secondPageOffsetTop - containerHeight / 4;
-//         } else {
-//             console.error('Second page element not found.');
-//             return false;
-//         }
-//     }
-
-//     // Attach scroll event listener to the container
-//     container.addEventListener('scroll', function() {
-//         // Check if scrolled to the second page
-//         if (isScrolledToSecondPage()) {
-//             // Change button color when scrolled to the second page
-//             header.style.backgroundColor = '#ffffff';
-//             // header.style.borderBottom = '1px solid #F4F4F4';
-//             // header.style.boxShadow = 'rgba(0, 0, 0, 0.1) 0px 2px 4px';
-//             button.style.color = '#ffffff';
-//             button.style.backgroundColor = '#000000'; // Change to desired color
-//             mlogo.src = 'Images/logo_black.png'; // Change to the second logo image URL
-//             nav.style.display = 'block'; // show the nav element
-//             logo.style.display = 'none'; // hide the logo element
-//         } else {
-//             // Revert button color to default when not scrolled to the second page
-//             header.style.backgroundColor = 'transparent';
-//             // header.style.borderBottom = 'none';
-//             // header.style.boxShadow = 'none';
-//             button.style.color = '#000000';
-//             button.style.backgroundColor = '#ffffff'; // Default button color
-//             mlogo.src = originalLogoSrc; // Revert to the original logo image
-//             nav.style.display = 'none'; // Hide the nav element
-//             logo.style.display = 'block'; // Show the logo element
-//         }
-//     });
-// });
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -203,41 +85,62 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Get reference to the button in the fixed header
-//     var header = document.getElementById('fixedHeader');
-//     var targetPage = document.getElementById('page3');
+//Theme-color-meta color change
 
-//     // Function to check if scrolled to the third page
-//     function isScrolledToThirdPage() {
-//         var thirdPage = document.querySelector('.pages.page3');
-//         if (thirdPage) {
-//             var containerHeight = window.innerHeight;
-//             var scrollTop = window.scrollY || window.pageYOffset;
-//             var thirdPageOffsetTop = thirdPage.offsetTop;
+document.addEventListener('DOMContentLoaded', function() {
+    var themeColorMeta = document.getElementById('theme-color-meta');
 
-//             return scrollTop >= thirdPageOffsetTop - containerHeight / 4;
-//         } else {
-//             console.error('Third page element not found.');
-//             return false;
-//         }
-//     }
+    function isScrolledTo95PercentOfPage2() {
+        var page2 = document.getElementById('page2');
+        if (page2) {
+            var page2Height = page2.offsetHeight;
+            var page2Top = page2.offsetTop;
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            var scrollThreshold = page2Top + (page2Height * 0.95); // 95% of page2 height
 
-//     // Function to handle scroll events
-//     function handleScroll() {
-//         // Check if scrolled to the second page
-//         if (isScrolledToThirdPage()) {
-//             // Change button color when scrolled to the second page
-//             header.style.backgroundColor = '#00ff00';
-//         }
-//     }
+            return scrollTop >= scrollThreshold;
+        } else {
+            console.error('Page 2 element not found.');
+            return false;
+        }
+    }
 
-//     // Initial call to handleScroll() to set initial state based on page load position
-//     handleScroll();
+    function isScrolledTo25PercentOfPage3() {
+        var page3 = document.getElementById('page3');
+        if (page3) {
+            var page3Top = page3.offsetTop;
+            var page3Height = page3.offsetHeight;
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            var scrollThreshold = page3Top + (page3Height * 0.25); // 25% of page3 height
 
-//     // Attach scroll event listener to the window
-//     window.addEventListener('scroll', handleScroll);
-// });
+            return scrollTop >= scrollThreshold;
+        } else {
+            console.error('Page 3 element not found.');
+            return false;
+        }
+    }
+
+    function handleScroll() {
+        var scrolledTo95PercentOfPage2 = isScrolledTo95PercentOfPage2();
+        var scrolledTo25PercentOfPage3 = isScrolledTo25PercentOfPage3();
+
+        if (scrolledTo95PercentOfPage2 && !scrolledTo25PercentOfPage3) {
+            // Change meta tag color to green when scrolled to 95% of page2 and not scrolled to 25% of page3
+            themeColorMeta.setAttribute('content', '#38FF27');
+        } else {
+            // Reset meta tag color to white
+            themeColorMeta.setAttribute('content', '#ffffff');
+        }
+    }
+
+    // Initial call to handleScroll() to set initial state based on page load position
+    handleScroll();
+
+    // Attach scroll event listener to the window
+    window.addEventListener('scroll', handleScroll);
+});
+
+//Fixed header color change to green page 3
 
 document.addEventListener('DOMContentLoaded', function() {
     // Get reference to the fixed header
