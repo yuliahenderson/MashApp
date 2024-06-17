777f17a8029f99592d52f42823bc914b11017606
Julia Henderson <yulia.p.henderson@gmail.com>
2024-06-17 13:37:00 -0400

    Add files via upload
    Updated site version June

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Function to detect if the device is mobile
-function isMobile() {
-    return window.matchMedia("(max-width: 768px)").matches;
+// Function to detect if the device is desktop
+function isDesktop() {
+    return window.matchMedia("(min-width: 993px)").matches;
 }
 
 // Ensure the code runs only when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    if (isMobile()) {
-        // Mobile-specific JavaScript functions
+    if (isDesktop()) {
+        console.log("Running desktop-specific scripts...");
+
+        // Desktop-specific JavaScript functions
 
         // Slider functionality
-        function mobileSliderFunction() {
+        function desktopSliderFunction() {
             const slider = document.querySelector('.slider');
             const slides = document.querySelectorAll('.slide');
             const containerHeight = document.querySelector('.container-phone').offsetHeight;
@@ -20,6 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 const img = slide.querySelector('img');
                 img.onload = function() {
                     slide.style.height = img.offsetHeight + 'px';
+                }
+                if (img.complete) { // Ensure height is set if image is already loaded
+                    img.onload();
                 }
             });
 
@@ -34,13 +39,169 @@ document.addEventListener("DOMContentLoaded", function () {
             setInterval(nextSlide, 5000);
         }
 
+function desktopFixedHeaderFunction() {
+    var header = document.getElementById('fixedHeader');
+    var mlogo = document.getElementById('mlogo');
+    var button = document.getElementById('scrollButton');
+    var nav = document.querySelector('nav');
+    var logo = document.getElementById('logo');
+
+    // Store the original logo source
+    var originalLogoSrc = mlogo.src;
+
+    function isScrollSnappedToPage2() {
+        var page2 = document.getElementById('page2');
+        if (page2) {
+            var rect = page2.getBoundingClientRect();
+            return rect.top >= 0 && rect.bottom <= window.innerHeight && Math.abs(rect.top) % window.innerHeight === 0;
+        }
+        return false;
+    }
+
+    function changeColorsAndLogo() {
+        if (isScrollSnappedToPage2()) {
+            // Change colors and logo when scroll-snapped to page2
+            header.style.backgroundColor = '#ffffff';
+            button.style.color = '#ffffff';
+            button.style.backgroundColor = '#000000';
+            mlogo.src = 'Images/logo_black.png';
+            mlogo.style = 'width="45px" height="36px';
+            nav.style.display = 'block';
+            logo.style.display = 'none';
+        } else {
+            // Revert colors and logo when not scroll-snapped to page2
+            header.style.backgroundColor = 'transparent';
+            button.style.color = '#000000';
+            button.style.backgroundColor = '#ffffff';
+            mlogo.src = originalLogoSrc;
+            // nav.style.display = 'none';
+            logo.style.display = 'block';
+        }
+    }
+
+    // Call the function to set initial state based on page load position
+    changeColorsAndLogo();
+
+    // Attach scroll event listener to the window
+    window.addEventListener('scroll', changeColorsAndLogo);
+}
+
+// Function to initialize the slider
+function initializeSlider() {
+  const slides = document.querySelectorAll('.slide4');
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.classList.add('active');
+        slide.classList.remove('inactive');
+        console.log(`Showing slide ${i}`); // Log current slide index
+      } else {
+        slide.classList.remove('active');
+        slide.classList.add('inactive');
+        console.log(`Hiding slide ${i}`); // Log non-active slides
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    console.log(`Next slide index: ${currentIndex}`); // Log next slide index
+    showSlide(currentIndex);
+  }
+
+  // Initial setup
+  showSlide(currentIndex);
+
+  // Start the loop
+  setInterval(nextSlide, 5000); // Change image every 5 seconds
+}
+
+// Check if the user is on "page4"
+if (window.location.hash === '#page4') {
+  // Execute initializeSlider function when user is on "page4"
+  initializeSlider();
+}
+
+
+
+        // Function to initialize all desktop-specific functionality
+        function initializeDesktopFunctions() {
+            desktopSliderFunction();
+            desktopFixedHeaderFunction();
+            initializeSlider()
+            // Call other desktop-specific functions here
+        }
+
+        // Initialize all desktop functions
+        initializeDesktopFunctions();
+    }
+});
+
+
+
+// Function to detect if the device is mobile
+function isMobile() {
+    return window.matchMedia("(max-width: 993px)").matches;
+}
+
+// Ensure the code runs only when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    if (isMobile()) {
+        console.log("Running mobile-specific scripts...");
+
+        // Mobile-specific JavaScript functions
+
+        // Slider functionality
+function mobileSliderFunction() {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    let index = 0;
+
+    // Function to handle image load event
+    function handleImageLoad() {
+        const slideHeight = this.offsetHeight; // Get the height of the loaded image
+        nextSlide(slideHeight); // Call nextSlide function with the slideHeight
+    }
+
+    // Attach load event listener to each image
+    slides.forEach(slide => {
+        const img = slide.querySelector('img');
+        img.addEventListener('load', handleImageLoad);
+    });
+
+    // Function to transition to the next slide
+    function nextSlide(slideHeight) {
+        index++;
+        if (index === slides.length) {
+            index = 0;
+        }
+        const translateY = -index * slideHeight + 'px';
+        slider.style.transform = `translateY(${translateY})`;
+    }
+
+    setInterval(() => {
+        // Start the slideshow once all images are loaded
+        const allImagesLoaded = Array.from(slides).every(slide => slide.querySelector('img').complete);
+        if (allImagesLoaded) {
+            const slideHeight = slides[0].querySelector('img').offsetHeight;
+            nextSlide(slideHeight);
+        }
+    }, 5000);
+}
+
+// Call the function when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', mobileSliderFunction);
+
         // Fixed header change on scroll
         function mobileFixedHeaderFunction() {
+
             var header = document.getElementById('fixedHeader');
-            var mlogo = document.getElementById('mlogo');
-            var button = document.getElementById('scrollButton');
-            var nav = document.querySelector('nav');
-            var logo = document.getElementById('logo');
+            var mlogo = document.querySelector('.mobile-only #mlogo');
+            var button = document.querySelector('.mobile-only #scrollButton');
+            var nav = document.querySelector('.mobile-only nav');
+            var logo = document.querySelector('.mobile-only #logo');
 
             // Store the original logo source
             var originalLogoSrc = mlogo.src;
@@ -59,6 +220,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             function changeColorsAndLogo() {
                 if (isScrolled5PercentOfPage1()) {
+                    console.log('Scrolled 5% of Page 1 - changing colors and logo');
                     // Change colors and logo when scrolled 5% of page1
                     header.style.backgroundColor = '#ffffff';
                     button.style.color = '#ffffff';
@@ -68,6 +230,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     nav.style.display = 'block';
                     logo.style.display = 'none';
                 } else {
+                    console.log('Not scrolled 5% of Page 1 - reverting colors and logo');
                     // Revert colors and logo when not scrolled 5% of page1
                     header.style.backgroundColor = 'transparent';
                     button.style.color = '#000000';
@@ -149,19 +312,60 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
 
+            function isScrolledTo95PercentOfPage10() {
+                var page10 = document.getElementById('page10');
+                if (page10) {
+                    var page10Height = page10.offsetHeight;
+                    var page10Top = page10.offsetTop;
+                    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                    var scrollThreshold = page10Top + (page10Height * 0.95); // 95% of page10 height
+
+                    return scrollTop >= scrollThreshold;
+                } else {
+                    console.error('Page 10 element not found.');
+                    return false;
+                }
+            }
+
+            function isScrolledTo95PercentOfPage11() {
+                var page11 = document.getElementById('page11');
+                if (page11) {
+                    var page11Top = page11.offsetTop;
+                    var page11Height = page11.offsetHeight;
+                    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                    var scrollThreshold = page11Top + (page11Height * 0.95); // 95% of page11 height
+
+                    return scrollTop >= scrollThreshold;
+                } else {
+                    console.error('Page 11 element not found.');
+                    return false;
+                }
+            }
+
             function handleScroll() {
                 var scrolledTo95PercentOfPage2 = isScrolledTo95PercentOfPage2();
                 var scrolledTo25PercentOfPage3 = isScrolledTo25PercentOfPage3();
                 var scrolledTo95PercentOfPage8 = isScrolledTo95PercentOfPage8();
                 var scrolledTo95PercentOfPage9 = isScrolledTo95PercentOfPage9();
+                var scrolledTo95PercentOfPage10 = isScrolledTo95PercentOfPage10();
+                var scrolledTo95PercentOfPage11 = isScrolledTo95PercentOfPage11();
 
                 if (scrolledTo95PercentOfPage2 && !scrolledTo25PercentOfPage3) {
+                    console.log('Scrolled to 95% of Page 2 and not 25% of Page 3 - changing meta color to green');
                     // Change meta tag color to green when scrolled to 95% of page2 and not scrolled to 25% of page3
                     themeColorMeta.setAttribute('content', '#38FF27');
                 } else if (scrolledTo95PercentOfPage8 && !scrolledTo95PercentOfPage9) {
+                    console.log('Scrolled to 95% of Page 8 and not 95% of Page 9 - changing meta color to grey');
                     // Change meta tag color to grey when scrolled to 95% of page8 and not scrolled to 95% of page9
                     themeColorMeta.setAttribute('content', '#f4f4f4');
-                } else {
+                }
+                  else if (scrolledTo95PercentOfPage10 && !scrolledTo95PercentOfPage11) {
+                    console.log('Scrolled to 95% of Page 10 and not 95% of Page 11 - changing meta color to blue');
+                    // Change meta tag color to blue when scrolled to 95% of page10 and not scrolled to 95% of page11
+                    themeColorMeta.setAttribute('content', '#00C9FF');
+                }
+                else {
+                    console.log('Resetting meta color to white');
                     // Reset meta tag color to white if none of the above conditions are met
                     themeColorMeta.setAttribute('content', '#ffffff');
                 }
@@ -213,6 +417,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 var scrolledTo25PercentOfPage3 = isScrolledTo25PercentOfPage3();
 
                 if (scrolledTo90PercentOfPage2 && !scrolledTo25PercentOfPage3) {
+                    console.log('Scrolled to 95% of Page 2 and not 25% of Page 3 - changing header color to green');
                     // Change header color to green if scrolled to 95% of page2 and not scrolled to 25% of page3
                     header.style.backgroundColor = '#38FF27';
                 }
@@ -224,6 +429,42 @@ document.addEventListener("DOMContentLoaded", function () {
             // Attach scroll event listener to the window
             window.addEventListener('scroll', handleScroll);
         }
+
+// Function to initialize the slider
+function initializeSlider() {
+  const slides = document.querySelectorAll('.slide4');
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.classList.add('active');
+        slide.classList.remove('inactive');
+      } else {
+        slide.classList.remove('active');
+        slide.classList.add('inactive');
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+
+  // Initial setup
+  showSlide(currentIndex);
+
+  // Start the loop
+  setInterval(nextSlide, 5000); // Change image every 5 seconds
+}
+
+// Check if the user is on "page4"
+if (window.location.hash === '#page4') {
+  // Execute initializeSlider function when user is on "page4"
+  initializeSlider();
+}
+
 
         // Fixed header color change to grey on page 9
         function mobileFixedHeaderGreyFunction() {
@@ -264,8 +505,61 @@ document.addEventListener("DOMContentLoaded", function () {
                 var scrolledTo95PercentOfPage9 = isScrolledTo95PercentOfPage9();
 
                 if (scrolledTo95PercentOfPage8 && !scrolledTo95PercentOfPage9) {
+                    console.log('Scrolled to 95% of Page 8 and not 95% of Page 9 - changing header color to grey');
                     // Change header color to grey if scrolled to 95% of page8 and not scrolled to 95% of page9
                     header.style.backgroundColor = '#f4f4f4';
+                }
+            }
+
+            // Initial call to handleScroll() to set initial state based on page load position
+            handleScroll();
+
+            // Attach scroll event listener to the window
+            window.addEventListener('scroll', handleScroll);
+        }
+
+// Fixed header color change to blue on page 11
+        function mobileFixedHeaderBlueFunction() {
+            var header = document.getElementById('fixedHeader');
+
+            function isScrolledTo95PercentOfPage10() {
+                var page10 = document.getElementById('page10');
+                if (page10) {
+                    var page10Height = page10.offsetHeight;
+                    var page10Top = page10.offsetTop;
+                    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                    var scrollThreshold = page10Top + (page10Height * 0.95); // 95% of page10 height
+
+                    return scrollTop >= scrollThreshold;
+                } else {
+                    console.error('Page 10 element not found.');
+                    return false;
+                }
+            }
+
+            function isScrolledTo25PercentOfPage11() {
+                var page11 = document.getElementById('page11');
+                if (page11) {
+                    var page11Top = page11.offsetTop;
+                    var page11Height = page11.offsetHeight;
+                    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                    var scrollThreshold = page11Top + (page11Height * 0.25); // 25% of page11 height
+
+                    return scrollTop >= scrollThreshold;
+                } else {
+                    console.error('Page 11 element not found.');
+                    return false;
+                }
+            }
+
+            function handleScroll() {
+                var scrolledTo95PercentOfPage10 = isScrolledTo95PercentOfPage10();
+                var scrolledTo25PercentOfPage11 = isScrolledTo25PercentOfPage11();
+
+                if (scrolledTo95PercentOfPage10 && !scrolledTo25PercentOfPage11) {
+                    console.log('Scrolled to 95% of Page 10 and not 25% of Page 11 - changing header color to blue');
+                    // Change header color to grey if scrolled to 95% of page8 and not scrolled to 95% of page9
+                    header.style.backgroundColor = '#00C9FF';
                 }
             }
 
@@ -282,13 +576,30 @@ document.addEventListener("DOMContentLoaded", function () {
             mobileFixedHeaderFunction();
             mobileThemeColorMetaFunction();
             mobileFixedHeaderGreenFunction();
+            initializeSlider()
             mobileFixedHeaderGreyFunction();
+            mobileFixedHeaderBlueFunction()
             // Call other mobile-specific functions here
         }
 
         // Initialize all mobile functions
         initializeMobileFunctions();
     }
+});
+
+
+        document.addEventListener('DOMContentLoaded', () => {
+        const collapsibles = document.querySelectorAll('.collapsible');
+
+    collapsibles.forEach(collapsible => {
+    const toggleIcon = collapsible.querySelector('.toggle-icon');
+    const hiddenContent = collapsible.querySelector('.hidden-content');
+
+    toggleIcon.addEventListener('click', () => {
+      hiddenContent.classList.toggle('active');
+      toggleIcon.classList.toggle('active');
+    });
+  });
 });
 
 
